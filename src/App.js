56f021572c4755006cfd1a567a1a329bb483f055
Yevheniia Kramar <104134459+ZhenkaUA@users.noreply.github.com>
2022-08-26 20:28:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <a href="#" className="btn btn-primary">
+        Hello world!
+      </a>
+    </div>
+  );
+}
+
+export default App;
