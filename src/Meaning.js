c2f56628e537984a7d3cfb2ecec0dd597410ba07
Yevheniia Kramar <104134459+ZhenkaUA@users.noreply.github.com>
2022-08-31 16:34:57 +0300

    Added Synonyms block

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props){
   if(props.meaning){
@@ -8,10 +9,12 @@ export default function Meaning(props){
     {props.meaning.definitions.map(function(definition,index)
     {
         return(
-            <div key={index}>
-    <p>{definition.definition}</p>
+    <div key={index}>
+    <p><strong>Definition:</strong> {definition.definition}
     <br/>
-    <em>{definition.example}</em>
+    <strong>Example: </strong><em>{definition.example}</em>
+    <Synonyms synonyms={definition.synonyms} />
+    </p>
     </div>
 );
 })}
