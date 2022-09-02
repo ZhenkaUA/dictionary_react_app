8cc298a7999b006207686405443f3e4074fff09a
Yevheniia Kramar <104134459+ZhenkaUA@users.noreply.github.com>
2022-09-03 01:10:54 +0300

    Added photo block

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,40 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props){
     let [keyword, setKeyword]=useState(props.defaultKeyword);
     let [definition, setDefinition]=useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos]=useState([]);
 
-function handleResponse(response){
-setDefinition(response.data[0]);
+function handleImages(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function handleResponse(response) {
+    setDefinition(response.data[0]);
+let apiKey = "563492ad6f91700001000001efeb45a99ade4d74ad800e399e1765b1";
+
+let apiUrl =`https://api.pexels.com/v1/search?query=${response.data[0].word}&per_page=6`;
+
+axios
+.get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+.then(handleImages);
+}
+
+
+function load(){
+    setLoaded(true);
+    search();
 }
 
 function search(){
 let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 }
+
 
 function handleSubmit(event){
     event.preventDefault();
@@ -26,10 +46,6 @@ function handleKeywordChange(event){
     setKeyword(event.target.value);  
 }
 
-function load(){
-    setLoaded(true);
-    search();
-}
 
 if (loaded){
     return (
@@ -41,6 +57,7 @@ if (loaded){
 </form>
 </section>
 <Results definition={definition}/>
+<Photos photos={photos}/>
     </div>
     );
 } else {
